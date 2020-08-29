dbca04113c9542c5e9dd0f5d5c6681b6cb6c1eb3
BoxiGod <DevirLand@gmail.com>
2020-08-29 08:30:05 +0300

    Update scratch_3.py
    Update

--- scratch_3.py
+++ scratch_3.py
@@ -1,6 +1,16 @@
 import xml.etree.ElementTree as ET
 
 
+def check_words(*name, words):
+    for text in name:
+        for word in text:
+            if len(word) > 6:
+                if word.lower() not in words.keys():
+                    words[word.lower()] = 1
+                else:
+                    words[word.lower()] += 1
+
+
 def main():
     words = {}
     parser = ET.XMLParser(encoding='utf-8')
@@ -8,24 +18,9 @@ def main():
     root = tree.getroot()
     all_news = root.findall('channel/item')
     for news in all_news:
-        title = news.find("title").text.split(" ")
-        description = news.find("description").text.split(" ")
-        for word in title:
-            if len(word) > 6:
-                if word not in words.keys():
-                    words[word] = 1
-                else:
-                    words[word] += 1
-        for word in description:
-            if len(word) > 6:
-                if word not in words.keys():
-                    words[word] = 1
-                else:
-                    words[word] += 1
-    for counter, key in enumerate(sorted(words, key=words.get, reverse=True)):
-        if counter < 10:
-            print(key, words[key])
-
+        check_words(news.find("title").text.split(" "), news.find("description").text.split(" "), words=words)
+    for counter, key in enumerate(sorted(words, key=words.get, reverse=True)[:10]):
+        print(key, words[key])
 
 
 main()
